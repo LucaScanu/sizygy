562a93a4113a995369fa6320d77f1b6dffe86241
LucaScanu <lucagscanu@gmail.com>
2017-05-26 10:01:38 +0100

    added text and current date to saved maths  button

--- js/app.js
+++ js/app.js
@@ -7,15 +7,16 @@ $(document).ready(function() {
   var operators = ["+", "-", "*", "/"];
   var dotOperator = ["."];
   var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var savedMaths = [""];
 
   function getValue(input) {
-    if(dotOperator.includes(inputs[inputs.length-1] === true && input === ".")) {
+    if(dotOperator.includes(inputs[inputs.length-1]) === true && input === ".") {
       console.log("Too many dots");
     }
     else if(inputs.length === 1 && operators.includes(input) === false) {
       inputs.push(input);
     }
-    else if(operators.includes(inputs[inputs.length-1] === false)) {
+    else if(operators.includes(inputs[inputs.length-1]) === false) {
       inputs.push(input);
     }
     else if(numbers.includes(Number(input))) {
@@ -31,8 +32,23 @@ $(document).ready(function() {
     console.log(totalString);
   }
   function getTotal() {
+    var currentResult = eval(totalString);
     totalString = inputs.join("");
     $("#result").html(eval(totalString));
+    $("#saveButton").on("click", function() {
+      savedMaths.push(currentResult);
+      var currentDate = new Date();
+      var today =
+      currentDate.getDate()+"-"+(currentDate.getMonth()+1)+"-"+currentDate.getFullYear();
+      var time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+      var date = today+" "+time;
+      var name = prompt("How would you like to call your result?");
+
+    $(".savedMath").text(""+name+". "+date+".");
+
+      console.log(name, date);
+    })
+
   }
 
   $("button").on("click", function() {
@@ -42,15 +58,14 @@ $(document).ready(function() {
     }
     else if(this.id === "total") {
       getTotal();
+      inputs = [""];
     }
     else {
       if(inputs[inputs.length-1].indexOf("+", "-", "*", "/", ".") === -1) {
-        getValue(this.id);
-        console.log(this.id);
+        getValue(this.value);
       }
       else {
-        getValue(this.id);
-        // console.log(this.id);
+        getValue(this.value);
       }
     }
   });
