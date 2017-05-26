ed96a9d47cde64dd7553524f7c369cb5cb2462e2
LucaScanu <lucagscanu@gmail.com>
2017-05-26 07:42:28 +0100

    functions display numbers

--- js/app.js
+++ js/app.js
@@ -1,50 +1,56 @@
 $(document).ready(function() {
 
   // storing user inputs
-  var entries = [""];
+  var inputs = [""];
 
   var totalString;
-
   var operators = ["+", "-", "*", "/"];
   var dotOperator = ["."];
   var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  function getValue(entry) {
-    if(dotOperator.includes(entries[entries.length-1] === true && entry === ".")) {
-      console.log("Duplicate '.'");
+  function getValue(input) {
+    if(dotOperator.includes(inputs[inputs.length-1] === true && input === ".")) {
+      console.log("Too many dots");
     }
-    else if(entries.length === 1 && operators.includes(entry) === false) {
-      entries.push(entry);
+    else if(inputs.length === 1 && operators.includes(input) === false) {
+      inputs.push(input);
     }
-    else if(operators.includes(entries[entries.length-1] === false)) {
-      
+    else if(operators.includes(inputs[inputs.length-1] === false)) {
+      inputs.push(input);
     }
+    else if(numbers.includes(Number(input))) {
+      inputs.push(input);
+    }
+    update();
   }
 
-  //this function will display the user entries in our result display
+  //this function will display the user inputs in our result display
   function update() {
-    totalString = entries.join("");
+    totalString = inputs.join("");
     $("#result").html(totalString);
+    console.log(totalString);
   }
   function getTotal() {
-    totalString = entries.join("");
+    totalString = inputs.join("");
     $("#result").html(eval(totalString));
   }
 
-  $("button").on('click', function() {
-    if(this.id === cancelButton) {
-      entries = [""];
+  $("button").on("click", function() {
+    if(this.id === "cancelButton") {
+      inputs = [""];
       update();
     }
-    else if(this.id === saveButton) {
+    else if(this.id === "total") {
       getTotal();
     }
     else {
-      if(entries[entries.length-1].indexOf("+", "-", "*", "/", ".") === -1) {
+      if(inputs[inputs.length-1].indexOf("+", "-", "*", "/", ".") === -1) {
         getValue(this.id);
+        console.log(this.id);
       }
       else {
         getValue(this.id);
+        // console.log(this.id);
       }
     }
   });
